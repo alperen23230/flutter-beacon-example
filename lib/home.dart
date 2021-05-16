166c81c4c45ad5c718d0d7e2eb635247e7013bbc
Alperen Ünal <alperenunal68@gmail.com>
2021-05-16 17:24:23 +0300

    Lib added

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_beacon/flutter_beacon.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,6 +8,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final regions = <Region>[];
+
+  @override
+  void initState() {
+    super.initState();
+    initializeBeacon();
+  }
+
+  void initializeBeacon() async {
+    try {
+      // if you want to manage manual checking about the required permissions
+      //await flutterBeacon.initializeScanning;
+
+      // or if you want to include automatic checking permission
+      final isScaning = await flutterBeacon.initializeAndCheckScanning;
+      print(isScaning);
+    } on PlatformException catch (e) {
+      // library failed to initialize, check code and message
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
